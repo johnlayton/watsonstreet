84163c18089562fb9b239f56f50a909698829a00
john layton <johnstewartlayton@gmail.com>
2016-09-04 21:20:39 +1000

    upgrade dependencies

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@
 #-L /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib >> ./lib/netcdf.js
 
 export LD_LIBRARY_PATH=/usr/local/Cellar/netcdf/4.3.3.1/lib
+
 export DEBUG_FD=3
 export DEBUG=netcdf
+
 node watsonstreet-test.js 3>&1 # debug.log
+
 ncdump -h new.nc
